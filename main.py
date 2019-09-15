3bd0c5507b9209ab2a7f0882ee8a432655dd9ea4
hgsgtk <hgsgtk@gmail.com>
2019-09-15 23:25:26 +0900

    start server by aiohttp

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """main function to start http server."""
 
 import argparse
+from aiohttp import web
 
 parser = argparse.ArgumentParser(description="PyCon JP 2019!")
 parser.add_argument('--port')
 args = parser.parse_args()
+
+app = web.Application()
+
+web.run_app(app, port=args.port)
